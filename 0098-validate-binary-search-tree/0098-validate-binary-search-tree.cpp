73f3df39e8e07ec87af06b23f25435807eda1f62
Roshan Kannojiya <roshanroshan60780@gmail.com>
2026-07-05 15:14:32 +0530

    Time: 0 ms (100%), Space: 21.9 MB (44.47%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -13,12 +13,8 @@ class Solution {
 private:
     bool helper(TreeNode* root , long long lower , long long upper){
         if(!root) return true;
-        if(root->left && (root->left->val >= root->val ||root->left->val <= lower)) return false;
-        if(root->right && (root->right->val <= root->val || root->right->val >= upper)) return false;
-        bool L=true , R=true;
-        if(root->left) L=helper(root->left , lower , root->val);
-        if(root->right) R=helper(root->right ,root->val, upper);
-        return  L && R ;
+        if(root->val >= upper || root->val <= lower) return false;
+        return  helper(root->left , lower , root->val) && helper(root->right ,root->val, upper) ;
     }
 public:
     bool isValidBST(TreeNode* root) {
