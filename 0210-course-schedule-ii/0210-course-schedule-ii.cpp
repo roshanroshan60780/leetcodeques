f58bbbf17d6f25134924997607a9dafd05dac732
Roshan Kannojiya <roshanroshan60780@gmail.com>
2026-07-10 17:48:42 +0530

    Time: 11 ms (11.61%), Space: 18.1 MB (83.38%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,38 +1,32 @@
 class Solution {
-private:
-    void trace(vector<vector<int>>& adj, vector<int>& visited, int curr , int& flag , vector<int>& ans) {
-        if (visited[curr]==1){
-            flag=1;
-            return;
-        }
-        if (visited[curr]==2)                //visited[curr]=0 : unvisited node
-            return;                          //visited[curr]=1 : node under recursion stack
-        visited[curr] = 1;                   //visited[curr]=2 : node is completely proces, means all childs are visited
-        int s = adj[curr].size();
-        for (int i = 0; i < s; i++) {
-            trace(adj, visited, adj[curr][i],flag,ans);
-            if (flag)
-                return;
-        }
-        visited[curr]=2;
-        ans.push_back(curr);
-        return;
-    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses);
         for (int i = 0; i < prerequisites.size(); i++) {
             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
         }
-        vector<int> ans;
-        int flag=0;
         vector<int> visited(numCourses,0);
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                trace(adj,visited,i,flag,ans);
-            }
-            if(flag) return {};
+        vector<int> toposort;
+        queue<int> q;
+        for(int i=0 ; i<numCourses ; i++){
+            if(indegree[i]==0){q.push(i);visited[i]=1;}
         }
-        return ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            toposort.push_back(node);
+            for(int x : adj[node]){
+                indegree[x]--;
+                if(indegree[x]==0 && !visited[x]){
+                    q.push(x);
+                    visited[x]=1;
+                }
+            }
+        }
+        reverse(toposort.begin(),toposort.end());
+        if(toposort.size()==numCourses) return toposort;
+        return {};
     }
 };
