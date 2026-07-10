90aa8af193e1c34eb8a8e0bc62befad7510c49d4
Roshan Kannojiya <roshanroshan60780@gmail.com>
2026-07-11 01:13:18 +0530

    Time: 54 ms (29.97%), Space: 66.9 MB (7.24%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -3,52 +3,34 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<vector<int>> rev(n);
-        vector<int> terminal;
-        unordered_set<int> st;
+        queue<int> q;
+        vector<int> indegree(n);
+        vector<int> visited(n);
         for (int i = 0; i < n; i++) {
             int s = graph[i].size();
+            indegree[i]=s;
             if (s == 0) {
-                terminal.push_back(i);
-                continue;
+                q.push(i);
+                visited[i]=1;
             }
             for (int j = 0; j < s; j++) {
                 rev[graph[i][j]].push_back(i);
             }
         }
-        if (terminal.empty())
-            return {};
-        queue<int> q;
-        vector<int> visited(n);
-        for (auto it : terminal) {
-            q.push(it);
-            st.insert(it);
-            visited[it] = 1;
-        }
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int node = q.front();
-                q.pop();
-                for (int it : rev[node]) {
-                    if (!visited[it]) {
-                        int flag = 1;
-                        for (int x : graph[it]) {
-                            if (st.find(x) == st.end()) {
-                                flag = 0;
-                                break;
-                            }
-                        }
-                        if (flag) {
-                            q.push(it);
-                            visited[it] = 1;
-                            st.insert(it);
-                            terminal.push_back(it);
-                        }
-                    }
+        vector<int>  safenode;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safenode.push_back(node);
+            for(int x : rev[node]){
+                indegree[x]--;
+                if(indegree[x]==0 && !visited[x]){
+                    q.push(x);
+                    visited[x]=1;
                 }
             }
         }
-        sort(terminal.begin(),terminal.end());
-        return terminal;
+        sort(safenode.begin(),safenode.end());
+        return safenode;
     }
 };
