e188d40c0462424b1f7bb5e8acae76a6d297baf7
Roshan Kannojiya <roshanroshan60780@gmail.com>
2026-07-10 17:41:02 +0530

    Time: 0 ms (100%), Space: 19.5 MB (45.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,30 @@
 class Solution {
-private:
-    bool detect(vector<vector<int>>& adj, vector<int>& visited, int curr) {
-        if (visited[curr]==1)
-            return false;
-        if (visited[curr]==2)                //visited[curr]=0 : unvisited node
-            return true;                     //visited[curr]=1 : node under recursion stack
-        visited[curr] = 1;                   //visited[curr]=2 : node is completely proces, means all childs are visited
-        int s = adj[curr].size();
-        bool ans = true;
-        for (int i = 0; i < s; i++) {
-            ans = ans && detect(adj, visited, adj[curr][i]);
-            if (!ans)
-                return false;
-        }
-        visited[curr]=2;
-        return true;
-    }
-
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses);
         for (int i = 0; i < prerequisites.size(); i++) {
             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
         }
         vector<int> visited(numCourses,0);
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (!detect(adj, visited, i))
-                    return false;
+        vector<int> toposort;
+        queue<int> q;
+        for(int i=0 ; i<numCourses ; i++){
+            if(indegree[i]==0){q.push(i);visited[i]=1;}
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            toposort.push_back(node);
+            for(int x : adj[node]){
+                indegree[x]--;
+                if(indegree[x]==0 && !visited[x]){
+                    q.push(x);
+                    visited[x]=1;
+                }
             }
         }
-        return true;
+        return toposort.size()==numCourses;
     }
 };
